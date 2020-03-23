e44e59f127db895f4f690868f357d89bd084ea9e
shubin <shubin@actimind.com>
2020-03-23 19:33:53 +0300

    change slider

--- src/modules/slider/slider.js
+++ src/modules/slider/slider.js
@@ -1,18 +1,20 @@
 $('.owl-carousel').owlCarousel({
-  loop:true,
+  loop:false,
   margin:0,
   nav:false,
-
   startPosition: 0,
   responsive:{
     0:{
-      items:1
+      items:1,
+      slideBy:1
     },
     600:{
-      items:1
+      items:1,
+      slideBy:1
     },
     993:{
-      items:2
+      items:2,
+      slideBy:2
     }
   }
 });
