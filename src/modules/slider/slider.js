11347fa39a244300fbf51b6e6e1abfef6c127584
shubin <shubin@actimind.com>
2020-03-24 14:24:16 +0300

    fixed slider

--- src/modules/slider/slider.js
+++ src/modules/slider/slider.js
@@ -1,12 +1,12 @@
 $('.owl-carousel').owlCarousel({
   loop:false,
-  margin:0,
+  margin: 0,
   nav:false,
   startPosition: 0,
   responsive:{
     0:{
       items:1,
-      slideBy:1
+      slideBy:1,
     },
     600:{
       items:1,
