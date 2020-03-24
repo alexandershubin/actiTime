e18ca618cffd407a1bd2399094801973525e02ff
shubin <shubin@actimind.com>
2020-03-24 14:08:12 +0300

    rewrite header plus add animation on menu

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import "owl.carousel2";
 if (process.env.NODE_ENV === 'development') {
   require('./index.html')
 }
-import Menu from './modules/header/header';
+// import Menu from './modules/header/header';
 import 'owl.carousel2/dist/assets/owl.carousel.css';
 import 'owl.carousel2/dist/assets/owl.theme.default.css';
 import './styles/style.scss';
 import './assets/img/logo.svg'
 import './modules/slider/slider'
-
-new Menu();
+import './modules/header/menu-burger';
+// new Menu();
 
 
 
